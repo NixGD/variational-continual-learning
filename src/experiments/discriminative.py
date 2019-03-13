6248add0b3f2231959f8bc481db78470a30f5466
marcellodebernardi <marcello1234@live.co.uk>
2019-03-13 13:14:19 +0000

    Add main script and a number of project files
    Source tree now consists of packages experiments, models, and util.

---
+++ src/experiments/discriminative.py
@@ -0,0 +1,14 @@
+import torch
+import numpy as np
+
+
+def permuted_mnist():
+    pass
+
+
+def split_mnist():
+    pass
+
+
+def split_not_mnist():
+    pass
---
+++ src/main.py
@@ -0,0 +1,26 @@
+import argparse
+import experiments.discriminative
+
+
+EXP_OPTIONS = {
+    'disc_p_mnist': experiments.discriminative.permuted_mnist,
+    'disc_s_mnist': experiments.discriminative.split_mnist,
+    'disc_s_n_mnist': experiments.discriminative.split_not_mnist,
+}
+
+
+def main(experiment='all'):
+    # run all experiments
+    if experiment == 'all':
+        for exp in list(EXP_OPTIONS.keys()):
+            EXP_OPTIONS[exp]()
+    # select specific experiment to run
+    else:
+        EXP_OPTIONS[experiment]()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('experiment', help='Experiment to be run, can be one of: ' + str(list(EXP_OPTIONS.keys())))
+    parser.parse_args()
+    main()
