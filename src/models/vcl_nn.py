890f2ba05afb67b6e67d81a2efeba8ff306bb125
Hadrien <pougeth@gmail.com>
2019-03-17 18:21:36 +0100

    Implemented loss function

--- src/models/vcl_nn.py
+++ src/models/vcl_nn.py
@@ -24,8 +24,13 @@ class VCL_NN(nn.Module):
         sampled_weights = w_means + w_epsilons * torch.sqrt(w_vars)
         sampled_bias = b_means + b_epsilons * torch.sqrt(b_vars)
 
+        # Apply each layer
         for i, layer in enumerate(sampled_weights):
             x = F.relu(layer @ x + sampled_bias[i])
+
+        # Apply final softmax
+        sm = torch.nn.Softmax(0)
+        x = sm(x)
 
         return x
 
@@ -34,14 +39,18 @@ class VCL_NN(nn.Module):
 
     def calculate_KL_term(self):
         '''Calculates and returns KL(posterior, prior). Formula from L3 slide 14.'''
-        # Concatenate w and b statistics into one tensor for ease of calculation
+        # Concatenate w and b statistics into one vector for ease of calculation
         ((prior_w_means, prior_w_vars), (prior_b_means, prior_b_vars)) = self.prior
-        prior_means = torch.cat((prior_w_means, prior_b_means), axis=0)
-        prior_vars = torch.cat((prior_w_vars, prior_b_vars), axis=0)
+        prior_means = torch.cat((torch.reshape(prior_w_means, (-1,)), 
+                                torch.reshape(prior_b_means, (-1,))), dim=0)
+        prior_vars  = torch.cat((torch.reshape(prior_w_vars, (-1,)), 
+                                torch.reshape(prior_b_vars, (-1,))), dim=0)
 
         ((post_w_means, post_w_vars), (post_b_means, post_b_vars)) = self.posterior
-        post_means = torch.cat((post_w_means, post_b_means), axis=0)
-        post_vars = torch.cat((post_w_vars, post_b_vars), axis=0)
+        post_means = torch.cat((torch.reshape(post_w_means, (-1,)), 
+                                torch.reshape(post_b_means, (-1,))), dim=0)
+        post_vars  = torch.cat((torch.reshape(post_w_vars, (-1,)), 
+                                torch.reshape(post_b_vars, (-1,))), dim=0)
 
         # Calculate KL for individual normal distributions over parameters
         KL_elementwise = \
@@ -52,17 +61,27 @@ class VCL_NN(nn.Module):
         # Sum KL over all parameters
         return 0.5 * KL_elementwise.sum()
 
-    def loss(self):
-        pass
+    def logprob(self, x, y):
+        # Make mask to select probabilities associated with actual y values
+        mask = torch.zeros(x.size(), dtype=torch.uint8)
+        for i in range(x.size()[1]):
+            mask[y[i].item()][i] = 1
+        
+        # Select probabilities, log and sum them
+        y_preds = torch.masked_select(self.forward(x), mask)
+        return sum(np.log(y_preds))
+
+    def loss(self, x, y):
+        return self.calculate_KL_term() - self.logprob(x, y)
 
     def init_prior(self):
-        if self.prior == None:
+        if self.posterior == None:
             w_means = torch.zeros(self.n_hidden_layers + 1,
-                                  self.layer_width, self.layer_width)
+                                self.layer_width, self.layer_width)
             w_vars = torch.ones(self.n_hidden_layers + 1,
                                 self.layer_width, self.layer_width)
-            b_means = torch.zeros(self.n_hidden_layers + 1, self.layer_width)
-            b_vars = torch.ones(self.n_hidden_layers + 1, self.layer_width)
+            b_means = torch.zeros(self.n_hidden_layers + 1, self.layer_width, 1)
+            b_vars = torch.ones(self.n_hidden_layers + 1, self.layer_width, 1)
             self.prior = ((w_means, w_vars), (b_means, b_vars))
         else:
             self.prior = self.posterior
