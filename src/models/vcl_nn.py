3fa173def13eedfd364dc2db42baf965bc97bac9
Adam Hillier <a.hillier@outlook.com>
2019-03-18 11:57:35 +0000

    Add correct variable initialisation

--- src/models/vcl_nn.py
+++ src/models/vcl_nn.py
@@ -14,7 +14,8 @@ class VCL_NN(nn.Module):
         self.n_hidden_layers = n_hidden_layers
         self.layer_width = layer_width
         self.prior, self.posterior = None, None
-        self.init_prior()
+        self.init_variables()
+        self.task_id = 0
 
     def forward(self, x):
         (w_means, w_vars), (b_means, b_vars) = self.posterior
@@ -25,8 +26,8 @@ class VCL_NN(nn.Module):
         sampled_bias = b_means + b_epsilons * torch.sqrt(b_vars)
 
         # Apply each layer
-        for i, layer in enumerate(sampled_weights):
-            x = F.relu(layer @ x + sampled_bias[i])
+        for weight, bias in zip(sampled_weights, sampled_bias):
+            x = F.relu(x @ weight + bias)
 
         # Apply final softmax
         sm = torch.nn.Softmax(0)
@@ -77,14 +78,53 @@ class VCL_NN(nn.Module):
     def loss(self, x, y):
         return self.calculate_KL_term() - self.logprob(x, y)
 
-    def init_prior(self):
-        if self.posterior == None:
-            w_means = torch.zeros(self.n_hidden_layers + 1,
-                                self.layer_width, self.layer_width)
-            w_vars = torch.ones(self.n_hidden_layers + 1,
-                                self.layer_width, self.layer_width)
-            b_means = torch.zeros(self.n_hidden_layers + 1, self.layer_width, 1)
-            b_vars = torch.ones(self.n_hidden_layers + 1, self.layer_width, 1)
-            self.prior = ((w_means, w_vars), (b_means, b_vars))
-        else:
-            self.prior = self.posterior
+    def reset_for_new_task(self):
+        """
+        Called after completion of a task, to reset state for the next task
+        """
+        self.task_id += 1
+
+        # Set the value of the prior to be the current value of the posterior
+        (prior_w_means, prior_w_vars), (prior_b_means, prior_b_vars) = self.prior
+        (posterior_w_means, posterior_w_vars), (posterior_b_means, posterior_b_vars) = self.prior
+        prior_w_means.data.copy_(posterior_w_means.data)
+        prior_w_vars.data.copy_(posterior_w_vars.data)
+        prior_b_means.data.copy_(posterior_b_means.data)
+        prior_b_vars.data.copy_(posterior_b_vars.data)
+
+    def init_variables(self):
+        """
+        Called once, on model creation, to set up the prior and posterior
+        tensors and set them to their initial values
+        """
+        # The first prior is initialised to be zero mean, unit variance
+        prior_w_means = torch.zeros(self.n_hidden_layers + 1, self.layer_width,
+                                    self.layer_width)
+        prior_w_vars  = torch.ones(self.n_hidden_layers + 1, self.layer_width,
+                                   self.layer_width)
+        prior_b_means = torch.zeros(self.n_hidden_layers + 1, self.layer_width)
+        prior_b_vars  = torch.ones(self.n_hidden_layers + 1, self.layer_width)
+
+        self.prior = ((prior_w_means, prior_w_vars), (prior_b_means, prior_b_vars))
+
+        # Prior tensors are registered as buffers to indicate to PyTorch that
+        # they are persistent state but shouldn't be updated by the optimiser
+        self.register_buffer("prior_w_means", prior_w_means)
+        self.register_buffer("prior_w_vars", prior_w_vars)
+        self.register_buffer("prior_b_means", prior_b_means)
+        self.register_buffer("prior_b_vars", prior_b_vars)
+
+        # The first posterior is initialised to be the same as the first prior
+        posterior_w_means = nn.Parameter(prior_w_means.clone().detach().requires_grad_(True))
+        posterior_w_vars  = nn.Parameter(prior_w_vars.clone().detach().requires_grad_(True))
+        posterior_b_means = nn.Parameter(prior_b_means.clone().detach().requires_grad_(True))
+        posterior_b_vars  = nn.Parameter(prior_b_vars.clone().detach().requires_grad_(True))
+
+        self.posterior = ((posterior_w_means, posterior_w_vars), (posterior_b_means, posterior_b_vars))
+
+        # Posterior tensors are registered as parameters to indicate to PyTorch
+        # that they are persistent state that should be updated by the optimiser
+        self.register_parameter("posterior_w_means", posterior_w_means)
+        self.register_parameter("posterior_w_vars", posterior_w_vars)
+        self.register_parameter("posterior_b_means", posterior_b_means)
+        self.register_parameter("posterior_b_vars", posterior_b_vars)
