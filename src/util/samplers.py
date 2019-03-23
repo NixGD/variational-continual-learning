43f9054a9446a0e157cdd635972e29fb0fef1f54
marcellodebernardi <marcello1234@live.co.uk>
2019-03-23 16:00:13 +0000

    Add split MNIST experiment code
    Experiment runner for the Split MNIST experiment. Includes a custom Sampler in util.samplers.

---
+++ src/util/samplers.py
@@ -0,0 +1,24 @@
+from torch.utils.data.sampler import Sampler
+
+
+class FilteringSampler(Sampler):
+    """
+    A Sampler which returns sequentially sampled elements from the given data source,
+    provided that their class label is part of a list of desired labels. That is, allows
+    sampling exclusively from a desired subset of classes.
+    """
+    def __init__(self, data_source, labels):
+        super().__init__(data_source)
+        self.data_source = data_source
+        self.labels = labels
+
+        unfiltered_indices = [idx if img[1] in self.labels else None for idx, img in enumerate(self.data_source, 0)]
+        self.indices = list(filter(lambda idx: idx is not None, unfiltered_indices))
+
+    def __iter__(self):
+        # must return a list of indices
+        return iter(self.indices)
+
+    def __len__(self):
+        # not really needed, implement if ever required
+        raise NotImplementedError()
