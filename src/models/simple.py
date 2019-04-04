5c6ccb71004c73350c0b06cf57c51d4f2bb11408
marcellodebernardi <marcello1234@live.co.uk>
2019-04-04 20:15:42 +0100

    Fix simple.py

--- src/models/simple.py
+++ src/models/simple.py
@@ -3,33 +3,28 @@ import torch.nn
 import torch.nn.functional as F
 
 
-class MLP(torch.nn.Module):
-    def __init__(self, in_size, out_size):
-        super().__init__()
-        self.linear_1 = torch.nn.Linear(in_size, 100)
-        self.linear_2 = torch.nn.Linear(100, 100)
-        self.linear_3 = torch.nn.Linear(100, out_size)
-        self.softmax = torch.nn.Softmax(dim=1)
-
-    def forward(self, x):
-        x = F.relu(self.linear_1(x))
-        x = F.relu(self.linear_2(x))
-        x = F.relu(self.linear_3(x))
-        x = self.softmax(x)
-        return x
-
-
 class MultiHeadMLP(torch.nn.Module):
-    def __init__(self, in_size, out_size, n_heads):
+    def __init__(self, x_dim, h_dim, y_dim, n_heads=1, shared_h_dims=(100, 100)):
         super().__init__()
+        # check for bad parameters
+        if n_heads < 1:
+            raise ValueError('Network requires at least one head.')
+
+        self.x_dim = x_dim
+        self.h_dim = h_dim
+        self.y_dim = y_dim
+        self.n_heads = n_heads
+
+        shared_dims = [x_dim] + list(shared_h_dims) + [h_dim]
+
+        # list of layers in shared network
         self.shared_layers = torch.nn.ModuleList([
-            torch.nn.Linear(in_size, 100),
-            torch.nn.Linear(100, 100),
-            torch.nn.Linear(100, 100)
+            torch.nn.Linear(shared_dims[i], shared_dims[i + 1]) for i in
+            range(len(shared_dims) - 1)
         ])
 
         self.heads = torch.nn.ModuleList([
-            torch.nn.Linear(100, out_size) for _ in range(n_heads)
+            torch.nn.Linear(self.h_dim, self.y_dim) for _ in range(n_heads)
         ])
 
         self.softmax = torch.nn.Softmax(dim=1)
@@ -39,8 +34,6 @@ class MultiHeadMLP(torch.nn.Module):
             x = F.relu(layer(x))
 
         x = self.heads[head_idx](x)
-        # for layer in self.heads[head_idx]:
-        #     x = layer(x)
 
         x = self.softmax(x)
         return x
@@ -50,8 +43,11 @@ class MultiHeadMLP(torch.nn.Module):
 
     def point_estimate_loss(self, x, y, head_idx):
         predictions = self(x, head_idx)
-        loss = torch.nn.CrossEntropyLoss()(predictions, y)
+        loss = torch.nn.NLLLoss()(predictions, y)
         return loss
 
     def vcl_loss(self, x, y, task_length, head_idx):
         self.point_estimate_loss(x, y, head_idx)
+
+    def reset_for_new_task(self):
+        pass
