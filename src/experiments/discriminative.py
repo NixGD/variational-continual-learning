80db11223e10ce8351924400ae92507a40aa01c7
NixGD <nixgdill@gmail.com>
2019-04-12 13:20:19 +0100

    change pairs in notMNIST to match paper

--- src/experiments/discriminative.py
+++ src/experiments/discriminative.py
@@ -183,11 +183,11 @@ def split_not_mnist():
 
     # todo: are the y classes integers?  Or characters?
     label_to_task_mapping = {
-        0: 0, 1: 0,
-        2: 1, 3: 1,
-        4: 2, 5: 2,
-        6: 3, 7: 3,
-        8: 4, 9: 4,
+        0: 0, 1: 1,
+        2: 2, 3: 3,
+        4: 4, 5: 0,
+        6: 1, 7: 2,
+        8: 3, 9: 4,
     }
 
     train_task_ids = torch.from_numpy(np.array([label_to_task_mapping[y] for _, y in not_mnist_train]))
